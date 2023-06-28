6d766e7e90b41cf11be517b15f7ed55ac45eec79
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2023-06-28 15:25:02 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -5,5 +5,20 @@ const router = express.Router();
 router.use('/add', (req,res,next) => {
     res.sendFile('/Users/ingaw/OneDrive/Desktop/Node/router/message.html')
 });
+router.use('/msg', (req,res,next) => {
+    
+    fs.appendFile("new.txt",` ${req.body.user}:${req.body.msg}` ,function (err) {
+        if (err) throw err;
+        console.log('Saved!1');
+      })
+      fs.readFile('new.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data)
+        res.send(`${data}<form id="btn" action="/msg" method="post"><input type="hidden" name="user" id="user"><input type="text" name ="msg" id ="msg"><button type="submit">click</button>  </form><script> document.getElementById('user').value=localStorage.getItem('username')</script>`) });
 
+      
+})
 module.exports = router;
